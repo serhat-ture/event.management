27328f3a61101340803b46da0b7d653d3b0a54c4
Serhat TURE <serhartture@gmail.com>
2023-04-02 02:15:32 +0300

    implemented database authentication

--- src/main/java/com/blc/eventManagement/config/SecurityConfig.java
+++ src/main/java/com/blc/eventManagement/config/SecurityConfig.java
@@ -2,12 +2,38 @@ package com.blc.eventManagement.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    private  UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 
 
     @Bean
@@ -15,9 +41,33 @@ public class SecurityConfig {
 
         http.csrf().disable()
                 .authorizeHttpRequests((authorize) ->
-                        authorize.anyRequest().authenticated())
+                      //  authorize.anyRequest().authenticated())
+                        authorize.requestMatchers(HttpMethod.GET,"/api/**").permitAll()
+                                .anyRequest().authenticated() )
                 .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
+
+
+
+
+ /*   @Bean
+    public UserDetailsService userDetailsService(){
+        UserDetails serhat= User.builder()
+                .username("serhat")
+                .password(passwordEncoder().encode( "serhat"))
+                .roles("USER")
+                .build();
+
+        UserDetails admin=User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode( "admin" ))
+                .roles("ADMIN")
+                .build();
+
+return  new InMemoryUserDetailsManager(serhat,admin);
+    }*/
+
+
 }
--- src/main/java/com/blc/eventManagement/controller/PersonController.java
+++ src/main/java/com/blc/eventManagement/controller/PersonController.java
@@ -5,6 +5,7 @@ import com.blc.eventManagement.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -18,6 +19,7 @@ public class PersonController {
         this.personService=personService;
     }
     // create person rest api
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping()
     public ResponseEntity<PersonDto> createPerson(@RequestBody PersonDto personDto) {
         PersonDto personResponse = personService.createPerson(personDto);
@@ -33,7 +35,7 @@ public class PersonController {
     public ResponseEntity<PersonDto> getPersonById(@PathVariable(name = "id") long id) {
         return ResponseEntity.ok(personService.getPersonById(id));
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PersonDto> updatePerson(@RequestBody PersonDto personDto, @PathVariable(name = "id") long id) {
         PersonDto personResponse = personService.updatePerson(personDto, id);
@@ -41,7 +43,7 @@ public class PersonController {
         return ResponseEntity.status(HttpStatus.OK).body(personResponse);
 
     }
-
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity deletePerson(@PathVariable(name = "id") Long id) {
         personService.deletePerson(id);
